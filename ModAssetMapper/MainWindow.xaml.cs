99c10f32bebe92f00d590fd71ee9bd25b233507d
Mator <why101@msn.com>
2016-04-20 20:49:46 -0700

    added window styling TODO: scroll bar looks ugly, fix it TODO: browse button hover looks ugly, fix it TODO: background thread for mapping task so the GUI will update actively

--- ModAssetMapper/MainWindow.xaml.cs
+++ ModAssetMapper/MainWindow.xaml.cs
@@ -9,6 +9,8 @@ using BA2Lib;
 using libbsa;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Windows.Documents;
+using System.ComponentModel;
 
 namespace ModAssetMapper {
 
@@ -26,22 +28,23 @@ namespace ModAssetMapper {
     public partial class MainWindow : Window {
         BA2NET ba2 = new BA2NET();
         BSANET bsa = new BSANET();
-        AssetFileList list;
+        AssetFileList list = new AssetFileList();
+        int visibility_mode = 0;
 
         public MainWindow() {
             InitializeComponent();
-            list = new AssetFileList();
         }
 
         public void HandleBA2(IArchiveEntry entry) {
             entry.WriteToDirectory(@".\\bsas", ExtractOptions.ExtractFullPath | ExtractOptions.Overwrite);
+            ProgressMessage("BSA extracted, Analyzing entries...");
             string rootPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string bsaPath = rootPath + "\\bsas\\" + entry.Key;
             if (ba2.Open(bsaPath)) {
                 string[] entries = ba2.GetNameTable();
                 for (int i = 0; i < entries.Length; i++) {
                     String entryPath = entry.Key + "\\" + entries[i];
-                    textBlock.Inlines.Add(entryPath + "\n");
+                    LogMessage(entryPath);
                     list.assets.Add(entryPath);
                 }
             }
@@ -55,37 +58,80 @@ namespace ModAssetMapper {
                 string[] entries = bsa.bsa_get_assets(".*");
                 for (int i = 0; i < entries.Length; i++) {
                     String entryPath = entry.Key + "\\" + entries[i];
-                    textBlock.Inlines.Add(entryPath + "\n");
+                    LogMessage(entryPath);
                     list.assets.Add(entryPath);
                 }
             }
         }
 
+        public void LogMessage(string message) {
+            if (textBlock.Inlines.Count == 0) {
+                textBlock.Inlines.Add(message + "\n");
+            }
+            else {
+                textBlock.Inlines.InsertBefore(textBlock.Inlines.FirstInline, new Run(message + "\n"));
+            }
+        }
+
+        public void ProgressMessage(string message) {
+            loading_label.Content = message;
+        }
+
         public void GetEntryMap(string path) {
             // load the archive
             var archive = ArchiveFactory.Open(@path);
+            ProgressMessage("Analyzing archive entries...");
             // loop through entries in archive
             foreach (var entry in archive.Entries) {
                 // for non-directory entries, store the 
                 if (!entry.IsDirectory) {
                     string entryPath = entry.Key;
                     list.assets.Add(entryPath);
-                    textBlock.Inlines.Add(entryPath + "\n");
+                    LogMessage(entryPath);
                     string ext = Path.GetExtension(entryPath);
                     if (String.Equals(ext, ".ba2", StringComparison.OrdinalIgnoreCase)) {
+                        ProgressMessage("Extracting BA2 at " + entryPath);
                         HandleBA2(entry);
+                        ProgressMessage("Analyzing archive entries...");
                     } else if (String.Equals(ext, ".bsa", StringComparison.OrdinalIgnoreCase)) {
+                        ProgressMessage("Extracting BSA at " + entryPath);
                         HandleBSA(entry);
+                        ProgressMessage("Analyzing archive entries...");
                     }
                 }
             }
+
+            // add padding to bottom of text block
+            textBlock.Inlines.Add("\n\n\n");
         }
 
-        private void button_Click(object sender, RoutedEventArgs e) {
+        private void toggleControlVisibility() {
+            // determine visibility modes
+            Visibility v1 = visibility_mode == 0 ? Visibility.Hidden : Visibility.Visible;
+            Visibility v2 = visibility_mode == 0 ? Visibility.Visible : Visibility.Hidden;
+
+            // toggle visibility of group 1
+            prompt_label.Visibility = v1;
+            logo.Visibility = v1;
+            browse_button.Visibility = v1;
+
+            // toggle visibility of group 2
+            loading_label.Visibility = v2;
+            scrollviewer.Visibility = v2;
+            reset_button.Visibility = v2;
+
+            // toggle mode
+            visibility_mode = (visibility_mode + 1) % 2;
+        }
+
+        private void browse_button_Click(object sender, RoutedEventArgs e) {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == true) {
+                // toggle control visibility
+                toggleControlVisibility();
+
                 // set filename
-                textBox.Text = openFileDialog.FileName;
+                ProgressMessage("Loading " + openFileDialog.FileName + "...");
 
                 // reset listing to empty
                 textBlock.Text = "";
@@ -95,8 +141,16 @@ namespace ModAssetMapper {
 
                 // write entry file tree to json file
                 String filename = Path.GetFileNameWithoutExtension(openFileDialog.FileName);
+                loading_label.Content = "Saving JSON to " + filename + ".json...";
                 File.WriteAllText(@".\" + filename + ".json", JsonConvert.SerializeObject(list));
+                loading_label.Content = "All done.  JSON file saved to "+ filename + ".json";
             }
+        }
+
+        private void reset_button_Click(object sender, RoutedEventArgs e) {
+            list.assets.Clear();
+            textBlock.Text = "";
+            toggleControlVisibility();
         }
     }
 }
