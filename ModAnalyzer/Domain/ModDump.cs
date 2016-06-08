f3005bb1e26d9d4c2e9af26c082c3d945d2a2684
Mator <why101@msn.com>
2016-06-08 15:20:48 -0700

    fixed bug with loading ModDumpLib.dll

--- ModAnalyzer/Domain/ModDump.cs
+++ ModAnalyzer/Domain/ModDump.cs
@@ -8,19 +8,19 @@ namespace ModAnalyzer.Domain
         /// <summary>
         /// Linking Delphi DLL for dumping plugins
         /// </summary>
-        [DllImport(@"lib\ModDumpLib.dll")]
+        [DllImport(@"ModDumpLib.dll")]
         public static extern void StartModDump();
-        [DllImport(@"lib\ModDumpLib.dll")]
+        [DllImport(@"ModDumpLib.dll")]
         public static extern void EndModDump();
-        [DllImport(@"lib\ModDumpLib.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(@"ModDumpLib.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         public static extern void GetBuffer(StringBuilder str, int len);
-        [DllImport(@"lib\ModDumpLib.dll")]
+        [DllImport(@"ModDumpLib.dll")]
         public static extern void FlushBuffer();
-        [DllImport(@"lib\ModDumpLib.dll")]
+        [DllImport(@"ModDumpLib.dll")]
         public static extern void SetGameMode(int mode);
-        [DllImport(@"lib\ModDumpLib.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(@"ModDumpLib.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         public static extern bool Prepare(string FilePath);
-        [DllImport(@"lib\ModDumpLib.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
+        [DllImport(@"ModDumpLib.dll", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         public static extern bool Dump(StringBuilder str, int len);
     }
 }
