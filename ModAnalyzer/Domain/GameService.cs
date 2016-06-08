c1fcd50d146707d7726d98449a698521dcc8b61a
Mator <why101@msn.com>
2016-06-08 15:44:10 -0700

    fixed game service game order, updated mod dump lib

--- ModAnalyzer/Domain/GameService.cs
+++ ModAnalyzer/Domain/GameService.cs
@@ -8,11 +8,11 @@ namespace ModAnalyzer
         public static Game game { get; set; }
 
         private static Game[] gameModes = {
-            new Game { longName = "Skyrim", gameName = "Skyrim", gameMode = 4, exeName = "TESV.exe", appIDs = "72850" },
-            new Game { longName = "Oblivion", gameName = "Oblivion", gameMode = 3, exeName = "Oblivion.exe", appIDs = "22330,900883" },
             new Game { longName = "Fallout New Vegas", gameName = "FalloutNV", gameMode = 0, exeName = "FalloutNV.exe", appIDs = "22380,2028016" },
             new Game { longName = "Fallout 3", gameName = "Fallout3", gameMode = 1, exeName = "Fallout3.exe", appIDs = "22300,22370" },
-            new Game { longName = "Fallout 4", gameName = "Fallout4", gameMode = 5, exeName = "Fallout4.exe", appIDs = "377160" }
+            new Game { longName = "Oblivion", gameName = "Oblivion", gameMode = 2, exeName = "Oblivion.exe", appIDs = "22330,900883" },
+            new Game { longName = "Skyrim", gameName = "Skyrim", gameMode = 3, exeName = "TESV.exe", appIDs = "72850" },
+            new Game { longName = "Fallout 4", gameName = "Fallout4", gameMode = 4, exeName = "Fallout4.exe", appIDs = "377160" }
         };
 
         public static Game getGame(String gameName) {
