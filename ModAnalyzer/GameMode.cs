6d6f8ec428dfef529d861437f3a98b954a9a2043
Mator <why101@msn.com>
2016-06-08 14:59:44 -0700

    added GameMode class

---
+++ ModAnalyzer/GameMode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ModAnalyzer
+{
+    public class Game 
+    {
+        public string longName { get; set; }
+        public string gameName { get; set; }
+        public int gameMode { get; set; }
+        public string appName { get; set; }
+        public string exeName { get; set; }
+        public string appIDs { get; set; }
+    }
+}
