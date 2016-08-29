e93a7dd6c2e0f04fcf67c48fe0ebe0da1b5442cf
Mator <why101@msn.com>
2016-08-29 15:14:14 -0700

    fixed bug with FomodFileNode constructor

--- ModAnalyzer/Domain/FomodFileNode.cs
+++ ModAnalyzer/Domain/FomodFileNode.cs
@@ -10,10 +10,14 @@ namespace ModAnalyzer.Domain {
         public string destination { get; set; }
         public int priority { get; set; }
 
-        public FomodFileNode(XmlNode node) {
-            source = node["source"].Value;
-            destination = node["destination"].Value;
-            priority = Int32.Parse(node["priority"].Value);
+        public FomodFileNode(XmlNode node)
+        {
+            source = node.Attributes["source"].Value;
+            destination = node.Attributes["destination"].Value;
+            if (node.Attributes["priority"] != null)
+            {
+                priority = Int32.Parse(node.Attributes["priority"].Value);
+            }
         }
     }
 }
