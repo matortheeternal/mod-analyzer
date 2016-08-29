48ba15aaad6847bf22492b06e1afe3493d0cd739
Mator <why101@msn.com>
2016-08-29 13:20:31 -0700

    created a FomodFileNode class for usage when parsing Fomods

---
+++ ModAnalyzer/Domain/FomodFileNode.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Xml;
+
+namespace ModAnalyzer.Domain {
+    /// <summary>
+    /// Used to store information on a fomod file or folder node
+    /// </summary>
+    public class FomodFileNode {
+        public string source { get; set; }
+        public string destination { get; set; }
+        public int priority { get; set; }
+
+        public FomodFileNode(XmlNode node) {
+            source = node["source"].Value;
+            destination = node["destination"].Value;
+            priority = Int32.Parse(node["priority"].Value);
+        }
+    }
+}
