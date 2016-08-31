4ca8be12671bee104b8337b0b6f3d387cbd69d84
Mator <why101@msn.com>
2016-08-31 12:44:09 -0700

    added FomodDependency class

---
+++ ModAnalyzer/Domain/FomodPattern.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ModAnalyzer.Domain {
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class FomodPattern {
+        public List<FomodFile> Files { get; }
+        public List<FomodFlagDependency> Dependencies { get; }
+
+        public FomodPattern(XmlNode node) {
+            XmlNode filesNode = node["files"];
+            if (filesNode != null) {
+                Files = FomodFile.FromNodes(filesNode.ChildNodes);
+            }
+
+            XmlNode dependenciesNode = node["dependencies"];
+            if (dependenciesNode != null) {
+                Dependencies = FomodFlagDependency.FromNodes(dependenciesNode.ChildNodes);
+            }
+        }
+
+        public static List<FomodPattern> FromDocument(XmlDocument doc) {
+            List<FomodPattern> patterns = new List<FomodPattern>();
+            foreach (XmlNode node in doc.GetElementsByTagName("pattern")) {
+                patterns.Add(new FomodPattern(node));
+            }
+            return patterns;
+        }
+    }
+}
