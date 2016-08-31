40f3b74aa9683084f91bed5192bd85b1637304a5
Mator <why101@msn.com>
2016-08-31 12:43:55 -0700

    added FomodFlagDependency class

---
+++ ModAnalyzer/Domain/FomodFlagDependency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ModAnalyzer.Domain {
+    /// <summary>
+    /// TODO
+    /// </summary>
+    public class FomodFlagDependency {
+        public string Flag { get; set; }
+        public string Value { get; set; }
+
+        public FomodFlagDependency(XmlNode node) {
+            Flag = node.Attributes["flag"].Value;
+            Value = node.Attributes["value"].Value;
+        }
+
+        public static List<FomodFlagDependency> FromNodes(XmlNodeList nodes) {
+            List<FomodFlagDependency> dependencies = new List<FomodFlagDependency>();
+            foreach (XmlNode node in nodes) {
+                dependencies.Add(new FomodFlagDependency(node));
+            }
+            return dependencies;
+        }
+    }
+}
