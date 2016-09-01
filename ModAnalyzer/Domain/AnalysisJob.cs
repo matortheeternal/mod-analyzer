8fbd28b487ca70f545c691cb564015ba267023b1
Mator <why101@msn.com>
2016-09-01 11:34:39 -0700

    created AnalysisJob class

---
+++ ModAnalyzer/Domain/AnalysisJob.cs
@@ -0,0 +1,35 @@
+﻿using SharpCompress.Archive;
+using System;
+using System.Collections.Generic;
+
+namespace ModAnalyzer.Domain {
+    /// <summary>
+    /// A job to analyze a BSA, BA2, ESP, or ESM in a mod.
+    /// </summary>
+    public class EntryAnalysisJob {
+        public IArchiveEntry Entry { get; set; }
+        public List<ModOption> Options { get; set; }
+
+        public EntryAnalysisJob(IArchiveEntry Entry, ModOption Option) {
+            this.Entry = Entry;
+            Options = new List<ModOption>();
+            Options.Add(Option);
+        }
+
+        public void AddOption(ModOption Option) {
+            Options.Add(Option);
+        }
+
+        public void AddAssetPaths(List<String> assetPaths) {
+            foreach (ModOption option in Options) {
+                option.Assets.AddRange(assetPaths);
+            }
+        }
+
+        public void AddPluginDump(PluginDump dump) {
+            foreach (ModOption option in Options) {
+                option.Plugins.Add(dump);
+            }
+        }
+    }
+}
