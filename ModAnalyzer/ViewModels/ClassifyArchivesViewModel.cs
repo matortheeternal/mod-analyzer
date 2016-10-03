07388b0a6bceb921665cc34da0a1a86e93cb3825
seesharpcode <seesharpcode@gmail.com>
2016-10-02 19:03:43 -0500

    fix to ensure at least 1 archive is marked 'Default'

--- ModAnalyzer/ViewModels/ClassifyArchivesViewModel.cs
+++ ModAnalyzer/ViewModels/ClassifyArchivesViewModel.cs
@@ -5,6 +5,7 @@ using ModAnalyzer.Messages;
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Windows;
 using System.Windows.Input;
 
 namespace ModAnalyzer.ViewModels {
@@ -14,20 +15,28 @@ namespace ModAnalyzer.ViewModels {
 
         public ClassifyArchivesViewModel() {
             ArchiveModOptions = new ObservableCollection<ModOption>();
-            AnalyzeCommand = new RelayCommand(() => MessengerInstance.Send(new ArchiveModOptionsSelectedMessage(ArchiveModOptions.ToList())));
+            AnalyzeCommand = new RelayCommand(AnalyzeMod);
 
             MessengerInstance.Register<FilesSelectedMessage>(this, OnFilesSelected);
         }
-
+                
         private void OnFilesSelected(FilesSelectedMessage message) {
             ArchiveModOptions.Clear();
-
+            
             foreach (string file in message.FilePaths)
                 ArchiveModOptions.Add(new ModOption(Path.GetFileName(file), false, false) { SourceFilePath = file });
 
             if (message.FilePaths.Count == 1) {
                 ArchiveModOptions.First().Default = true;
                 MessengerInstance.Send(new ArchiveModOptionsSelectedMessage(ArchiveModOptions.ToList()));
+            }
+        }
+
+        private void AnalyzeMod() {
+            if (ArchiveModOptions.Any(modOption => modOption.Default)) {
+                MessengerInstance.Send(new ArchiveModOptionsSelectedMessage(ArchiveModOptions.ToList()));
+            } else {
+                MessageBox.Show("Please specify at least 1 default archive.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
             }
         }
     }
