93d3ef83ec739dc4e5bc95bf7e5e4b553ea8ac05
Mator <why101@msn.com>
2016-12-02 16:46:32 -0800

    created FomodArchiveService

---
+++ ModAnalyzer/Domain/FomodArchiveService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using SharpCompress.Archive;
+using System.Collections.Generic;
+using ModAnalyzer.Utils;
+
+namespace ModAnalyzer.Domain {
+    public static class FomodArchiveService {
+        public static IArchiveEntry GetFomodConfig(IArchive archive) {
+            return archive.FindArchiveEntry(@"fomod\ModuleConfig.xml");
+        }
+
+        public static string GetBasePath(string configEntryPath) {
+            string configFomodPath = @"fomod\ModuleConfig.xml";
+            int index = configEntryPath.IndexOf(configFomodPath, StringComparison.OrdinalIgnoreCase);
+            if (index >= 0) {
+                return configEntryPath.Remove(index, configFomodPath.Length);
+            }
+            else {
+                return "";
+            }
+        }
+    }
+}
