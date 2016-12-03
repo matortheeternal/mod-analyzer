e75238183cd55330308d37d46db9b176b2ddd3a5
Mator <why101@msn.com>
2016-12-02 16:46:09 -0800

    created PathExtensions

---
+++ ModAnalyzer/Utils/PathExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace ModAnalyzer.Utils {
+    
+    public static class PathExtensions {
+        public static int GetLevel(string path) {
+            return path.Split('\\').Length;
+        }
+
+        public static string AppendDelimiter(string path) {
+            if (path.EndsWith(@"\")) {
+                return path;
+            }
+            else {
+                return path + @"\";
+            }
+        }
+
+        public static string FixDelimiters(string path) {
+            return path.Replace("/", @"\");
+        }
+    }
+}
