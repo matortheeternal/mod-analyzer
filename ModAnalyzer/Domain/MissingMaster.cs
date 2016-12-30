53f24cdd20c35f5513ea333fea9b81fa37cf7dfa
Mator <why101@msn.com>
2016-12-29 18:00:53 -0800

    created MissingMaster class

---
+++ ModAnalyzer/Domain/MissingMaster.cs
@@ -0,0 +1,15 @@
+﻿namespace ModAnalyzer {
+    public class MissingMaster {
+        public string FileName { get; set; }
+        public string RequiredBy { get; set; }
+
+        public MissingMaster(string filename, string requiredBy) {
+            FileName = filename;
+            RequiredBy = requiredBy;
+        }
+
+        public void AddRequiredBy(string pluginPath) {
+            RequiredBy += ";" + pluginPath;
+        }
+    }
+}
