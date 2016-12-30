6e5947d9363a2eed85fff4f9e9e5d34747f5a9bc
Mator <why101@msn.com>
2016-12-29 17:59:36 -0800

    created new view for viewing missing plugin masters

---
+++ ModAnalyzer/ViewModels/PluginMastersViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using ModAnalyzer.Domain;
+using ModAnalyzer.Messages;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ModAnalyzer.ViewModels {
+    public class PluginMastersViewModel : ViewModelBase {
+        public ObservableCollection<MissingMaster> MissingMasters { get; set; }
+        public List<ModOption> ModOptions { get; set; }
+        public ICommand ContinueCommand { get; set; }
+
+        public PluginMastersViewModel() {
+            MissingMasters = new ObservableCollection<MissingMaster>();
+            ContinueCommand = new RelayCommand(Continue);
+            MessengerInstance.Register<MissingMastersMessage>(this, OnMissingMasters);
+        }
+
+        private void OnMissingMasters(MissingMastersMessage message) {
+            foreach (var item in message.MissingMasters) MissingMasters.Add(item);
+            ModOptions = message.ModOptions;
+        }
+
+        private void Continue() {
+            MessengerInstance.Send(new AnalyzeArchivesMessage(ModOptions));
+        }
+
+        public void Back() {
+            MessengerInstance.Send(new NavigationMessage(Page.ClassifyArchives));
+        }
+    }
+}
---
+++ ModAnalyzer/Views/PluginMastersView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using ModAnalyzer.ViewModels;
+using System.Windows.Controls;
+
+namespace ModAnalyzer.Views {
+    public partial class PluginMastersView : UserControl {
+        public PluginMastersView() {
+            InitializeComponent();
+        }
+
+        private void PackIcon_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e) {
+            ((PluginMastersViewModel) DataContext).Back();
+        }
+    }
+}
