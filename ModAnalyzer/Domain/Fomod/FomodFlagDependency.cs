87213b7de52ae19ed1b4a526aee03664b432cdce
Mator <why101@msn.com>
2017-05-25 15:56:41 -0700

    slight adjustment to how some code is written in FomodFlagDependency

--- ModAnalyzer/Domain/Fomod/FomodFlagDependency.cs
+++ ModAnalyzer/Domain/Fomod/FomodFlagDependency.cs
@@ -15,9 +15,8 @@ namespace ModAnalyzer.Domain.Fomod {
         public static List<FomodFlagDependency> FromNodes(XmlNodeList nodes) {
             List<FomodFlagDependency> dependencies = new List<FomodFlagDependency>();
             foreach (XmlNode node in nodes) {
-                if (node.Name == "flagDependency") {
-                    dependencies.Add(new FomodFlagDependency(node));
-                }
+                if (node.Name != "flagDependency") continue;
+                dependencies.Add(new FomodFlagDependency(node));
             }
             return dependencies;
         }
