c87f969fa9426645aba5c822da1397376f60e47a
Colin <why101@msn.com>
2018-06-06 19:57:56 -0700

    fixed bug with mapping FomodFiles to empty string destinations

--- ModAnalyzer/Domain/Fomod/FomodFile.cs
+++ ModAnalyzer/Domain/Fomod/FomodFile.cs
@@ -50,6 +50,7 @@ namespace ModAnalyzer.Domain.Fomod {
         }
 
         public string MappedPath(string path) {
+            if (string.IsNullOrEmpty(Destination)) return path.Replace("/", @"\");
             return Expr.Replace(path, Destination).Replace("/", @"\");
         }
     }
